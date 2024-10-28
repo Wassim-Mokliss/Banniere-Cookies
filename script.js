93d8ea1681d2d43d507fec4dff4d9a6aacdf17a6
Wassim-Mokliss <wasshimudev@gmail.com>
2024-10-28 17:52:00 +0100

    AJout css + stockage div cookie et ajout evenement au click

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log('connecté !')
+// Je selectionnet je stock
+// Bouton succes
+const btnSuccess = document.querySelector('.btn-succes');
+
+// DIV cookies
+const cookies = document.querySelector('.cookies');
+
+// Je soumet un evenement lors du click sur le bouton accepter cookies
+btnSuccess.addEventListener('click', function(){
+    console.log('bouton clické')
+});
